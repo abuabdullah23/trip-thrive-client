dedd2a8e56b8c2c81f52c959628d3b7b139d35a3
abuabdullah23 <abuobaidazayed@gmail.com>
2023-11-08 23:19:31 +0600

    Handle error when not found data from api

--- src/components/ServicesCard/ServicesCard.jsx
+++ src/components/ServicesCard/ServicesCard.jsx
@@ -6,29 +6,33 @@ const ServicesCard = ({ singleService }) => {
     const { _id, service, image, price, providerName, providerEmail, providerPhoto, location, description } = singleService;
 
     return (
-        <div className='flex flex-col gap-2 border border-rose-200 rounded-lg overflow-hidden text-slate-700'>
-            <div className='relative'>
-                <img className='h-[196px] w-full object-cover relative' src={image} alt="service image" />
+        <div className='flex flex-col lg:flex-row gap-2 lg:gap-3 transition-all duration-300 border border-rose-200 rounded-lg overflow-hidden text-slate-700'>
+            <div className='w-full lg:w-1/3'>
+                <img className='h-full w-full object-cover relative' src={image} alt="service image" />
+            </div>
 
-                <div className='flex items-center gap-3 p-3 text-white w-full bg-gray-950/90 absolute left-0 bottom-0'>
+            <div className='px-2 py-1 flex flex-col gap-[6px] mb-2 w-full lg:w-2/3'>
+                <h2 className='text-lg font-bold leading-tight'>{service}</h2>
+                <p>{description.slice(0, 60)}...</p>
+
+                <div className='flex items-center gap-3'>
                     <img title='Service provider photo' className='h-8 w-8 border border-rose-400 rounded-full' src={providerPhoto} alt="user photo" />
 
                     <div className='flex flex-col'>
                         <span title='Provider name' className='text-xl font-semibold'>{providerName}</span>
                     </div>
                 </div>
-            </div>
 
-            <div className='px-3 flex flex-col gap-2 mb-2'>
-                <h2 className='text-lg font-bold leading-tight'>{service}</h2>
-                <p className='text-red-600 text-lg font-bold'><span className='text-base font-normal text-gray-600'>Price:</span> ${price}</p>
-                <p>{description}</p>
-                <Link to={`/service-details/${_id}`} className='w-full py-1 rounded text-white text-center font-normal bg-rose-500 hover:bg-rose-600 flex items-center gap-1 hover:gap-2 transition-all duration-300 justify-center'>
-                    <span>View Details</span>
-                    <BsArrowRightShort className='text-2xl mt-[2px]' />
-                </Link>
-            </div>
+                <div className='flex items-center justify-between gap-2 pr-1'>
+                    <p className='text-rose-500 text-lg font-bold'><span className='font-normal text-gray-600'>Price:</span> ${price}</p>
+                    
+                    <Link to={`/service-details/${_id}`} className='w-fit mt-2 py-1 px-3 rounded text-white text-center font-normal bg-rose-500 hover:bg-rose-600 flex items-center gap-1 hover:gap-2 transition-all duration-300 justify-center'>
+                        <span>View Details</span>
+                        <BsArrowRightShort className='text-2xl mt-[2px]' />
+                    </Link>
 
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Home/PopularServices/PopularServices.jsx
+++ src/pages/Home/PopularServices/PopularServices.jsx
@@ -2,6 +2,7 @@ import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 import ServicesCard from '../../../components/ServicesCard/ServicesCard';
 import { Link } from 'react-router-dom';
 import useAllServices from '../../../hook/useAllServices';
+import EmptyContent from '../../../components/EmptyContent/EmptyContent';
 
 const PopularServices = () => {
     const allServices = useAllServices();
@@ -10,18 +11,28 @@ const PopularServices = () => {
         <div className='py-12'>
             <SectionTitle sectionTitle={'Popular Services'} />
 
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+            <>
                 {
-                    allServices?.slice(0, 4).map((services, i) => <ServicesCard
-                        key={i}
-                        singleService={services}
-                    />)
-                }
-            </div>
+                    allServices?.length > 0 ? <>
+                        <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+                            {
+                                allServices?.slice(0, 4).map((services, i) => <ServicesCard
+                                    key={i}
+                                    singleService={services}
+                                />)
+                            }
+                        </div>
 
-            <div className='mt-12 flex items-center justify-center'>
-                <Link to='/services' className='py-2 px-8 border border-rose-500 hover:bg-rose-500 hover:text-white rounded font-semibold transition-all duration-300'>Show All</Link>
-            </div>
+                        <div className='mt-12 flex items-center justify-center'>
+                            <Link to='/services' className='py-2 px-8 border border-rose-500 hover:bg-rose-500 hover:text-white rounded font-semibold transition-all duration-300'>Show All</Link>
+                        </div>
+                    </> : <>
+                        <div className='py-8'>
+                            <EmptyContent emptyText={'Server Error, Services Not Found!'} />
+                        </div>
+                    </>
+                }
+            </>
         </div>
     );
 };
